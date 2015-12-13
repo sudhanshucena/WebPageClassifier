be6383e74baff759fd35f4dfdca61ba23124b3ed
sudhanshucena <sudhanshucena@gmail.com>
2015-12-13 16:28:00 +0000

    changes to test case

--- src/test/scala-2.11/UnitTest.scala
+++ src/test/scala-2.11/UnitTest.scala
@@ -7,16 +7,16 @@
 import org.scalatest._
 
 class UnitTest extends FlatSpec  {
-  "A HTMLContentExtractor " should
-    "return the contents of HTML page as string" in {
-
-      val url: String = "http://www.bbc.co.uk/news/uk-35019021"
-      val actual = new HtmlContentExtractor().content(url)//WebPageClassifier.textExtractor.(url)
-//    val foo = actual.toString
-//    val integ = 1
-      assert(actual != null)
-
-  }
+//  "A HTMLContentExtractor " should
+//    "return the contents of HTML page as string" in {
+//
+//      val url: String = "http://www.bbc.co.uk/news/uk-35019021"
+//      val actual = new HtmlContentExtractor().content(url)//WebPageClassifier.textExtractor.(url)
+////    val foo = actual.toString
+////    val integ = 1
+//      assert(actual != null)
+//
+//  }
 
   "A List(String Object) " should
     "return the words in the String as a List" in {
